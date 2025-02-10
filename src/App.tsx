13d8457056d4bdfe67649e8ebec0d36a60f7d242
root <smail.abud@gmail.com>
2025-02-10 05:55:46 +0000

    feat: add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { useRoutes, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import routes from "tempo-routes";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
         {import.meta.env.VITE_TEMPO === "true" && useRoutes(routes)}
+        <Analytics />
       </>
     </Suspense>
   );
